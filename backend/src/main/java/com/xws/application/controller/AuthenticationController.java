89aa6f2a4c05c409956a5664c87ee657765bce89
kacaa <katarina.aleksic97@gmail.com>
2020-06-17 00:30:55 +0200

    Login added

---
+++ backend/src/main/java/com/xws/application/controller/AuthenticationController.java
@@ -0,0 +1,65 @@
+package com.xws.application.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xws.application.security.JwtAuthenticationRequest;
+import com.xws.application.security.TokenUtils;
+import com.xws.application.service.CustomUserDetailsService;
+
+//Kontroler zaduzen za autentifikaciju korisnika
+@RestController
+@RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AuthenticationController {
+
+	@Autowired
+	TokenUtils tokenUtils;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<String> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest,
+			HttpServletResponse response) throws AuthenticationException, IOException {
+
+		UserDetails user = null;
+		final Authentication authentication;
+		try {
+			user = this.userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+			authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+						authenticationRequest.getPassword()));
+		}
+		catch(UsernameNotFoundException e) {
+			return new ResponseEntity<String>(e.getMessage(),HttpStatus.NOT_ACCEPTABLE);
+		}
+		catch(BadCredentialsException e) {
+			return new ResponseEntity<String>("Wrong password",HttpStatus.NOT_ACCEPTABLE);
+		}
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		String jwt = tokenUtils.generateToken(user);
+		return new ResponseEntity<String>(jwt, HttpStatus.OK);
+	}
+}
--- backend/src/main/java/com/xws/application/repository/XMLDBManager.java
+++ backend/src/main/java/com/xws/application/repository/XMLDBManager.java
@@ -1,20 +1,24 @@
 package com.xws.application.repository;
 
-import com.xws.application.parser.JAXB;
-import com.xws.application.util.AuthenticationUtilities;
-import org.exist.xmldb.EXistResource;
-import org.xmldb.api.DatabaseManager;
-import org.xmldb.api.base.Collection;
-import org.xmldb.api.base.Database;
-import org.xmldb.api.base.XMLDBException;
-import org.xmldb.api.modules.CollectionManagementService;
-import org.xmldb.api.modules.XMLResource;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.OutputKeys;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
+
+import org.exist.xmldb.EXistResource;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.ResourceSet;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.CollectionManagementService;
+import org.xmldb.api.modules.XMLResource;
+import org.xmldb.api.modules.XPathQueryService;
+
+import com.xws.application.parser.JAXB;
+import com.xws.application.util.AuthenticationUtilities;
 
 public class XMLDBManager {
 
@@ -188,5 +192,112 @@ public class XMLDBManager {
 
 		return model;
 	}
+	
+	 public static Collection getOrCreateCollection(AuthenticationUtilities.ConnectionProperties conn, String collectionUri) throws XMLDBException {
+	        return getOrCreateCollection(conn,collectionUri, 0);
+	    }
+
+    private static Collection getOrCreateCollection(AuthenticationUtilities.ConnectionProperties conn, String collectionUri, int pathSegmentOffset) throws XMLDBException {
+        
+        Collection col = DatabaseManager.getCollection(conn.uri + collectionUri, conn.user, conn.password);
+        
+        // create the collection if it does not exist
+        if(col == null) {
+         	if(collectionUri.startsWith("/")) {
+                collectionUri = collectionUri.substring(1);
+            }
+            
+        	String pathSegments[] = collectionUri.split("/");
+            
+        	if(pathSegments.length > 0) {
+                StringBuilder path = new StringBuilder();
+            
+                for(int i = 0; i <= pathSegmentOffset; i++) {
+                    path.append("/" + pathSegments[i]);
+                }
+                
+                Collection startCol = DatabaseManager.getCollection(conn.uri + path, conn.user, conn.password);
+                
+                if (startCol == null) {
+                	
+                	// child collection does not exist
+                    
+                	String parentPath = path.substring(0, path.lastIndexOf("/"));
+                    Collection parentCol = DatabaseManager.getCollection(conn.uri + parentPath, conn.user, conn.password);
+                    
+                    CollectionManagementService mgt = (CollectionManagementService) parentCol.getService("CollectionManagementService", "1.0");
+                    
+                    System.out.println("[INFO] Creating the collection: " + pathSegments[pathSegmentOffset]);
+                    col = mgt.createCollection(pathSegments[pathSegmentOffset]);
+                    
+                    col.close();
+                    parentCol.close();
+                    
+                } else {
+                    startCol.close();
+                }
+            }
+            return getOrCreateCollection(conn, collectionUri, ++pathSegmentOffset);
+        } else {
+            return col;
+        }
+    }
+	
+	public static ResourceSet retrieveWithXPath(String collectionId, String xpathExp, String TARGET_NAMESPACE) throws Exception {
+		AuthenticationUtilities.ConnectionProperties conn = AuthenticationUtilities.loadProperties();
+
+		// initialize collection and document identifiers
+		System.out.println("\t- collection ID: " + collectionId);
+
+		// initialize database driver
+		System.out.println("[INFO] Loading driver class: " + conn.driver);
+		Class<?> cl = Class.forName(conn.driver);
+
+		Database database = (Database) cl.newInstance();
+		database.setProperty("create-database", "true");
+
+		DatabaseManager.registerDatabase(database);
+
+		Collection col = null;
+		ResourceSet result = null;
+//
+//		Object model = null;
+
+		try {
+			// get the collection
+			System.out.println("[INFO] Retrieving the collection: " + collectionId);
+			col = getOrCreateCollection(conn, collectionId);
+
+            if (col == null) {
+                return null;
+            }
+         // get an instance of xpath query service
+            XPathQueryService xpathService = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+            xpathService.setProperty("indent", "yes");
+
+            // make the service aware of namespaces, using the default one
+            xpathService.setNamespace("", TARGET_NAMESPACE);
+            // System.out.println("\n[INPUT] Enter an XPath expression (e.g.
+            // doc(\"1.xml\")//book[@category=\"WEB\" and price>35]/title): ");
+            // execute xpath expression
+            System.out.println("[INFO] Invoking XPath query service for: " + xpathExp);
+            result = xpathService.query("collection(\"/db/library/users\")/users/user[userInfo/email=\"0@00.AA\"]");
+            // handle the results
+            System.out.println("[INFO] Handling the results... ");
+		} finally {
+			//don't forget to clean up!
+
+			// don't forget to cleanup
+            if(col != null) {
+                try { 
+                	col.close();
+                } catch (XMLDBException xe) {
+                	xe.printStackTrace();
+                }
+			}
+		}
+		return result;
+	}
+
 
 }
